e5df47b0ec59b2f6eac7eab528b7f2876b55776d
suneric <syf_more@163.com>
2020-12-14 13:24:47 -0500

    revert to one file without split env and sensors in differnt file

--- do_training/scripts/envs/door_open_task_env.py
+++ do_training/scripts/envs/door_open_task_env.py
@@ -10,11 +10,12 @@ from std_msgs.msg import Float64
 from gazebo_msgs.msg import LinkStates, ModelStates, ModelState, LinkState
 from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import Pose, Twist
+from sensor_msgs.msg import Image
 import tf.transformations as tft
-import math
 import cv2
-from .sensors import CameraSensor, PoseSensor
-from .robot_driver import RobotDriver
+from cv_bridge import CvBridge, CvBridgeError
+import math
+import skimage
 
 class ModelSaver:
     def __init__(self,freq):
@@ -28,6 +29,163 @@ class ModelSaver:
             net.save(model_path)
             print("save trained model:", model_path)
 
+"""
+CameraSensor with resolution, topic and guassian noise level by default variance = 0.05, mean = 0.0
+"""
+class CameraSensor():
+    def __init__(self, resolution=(64,64), topic='/cam_front/image_raw', noise=0):
+        self.resolution = resolution
+        self.topic = topic
+        self.noise = noise
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber(self.topic, Image, self._image_cb)
+        self.rgb_image = None
+        self.grey_image = None
+
+    def _image_cb(self,data):
+        try:
+            image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.rgb_image = self._guass_noisy(image, self.noise)
+            # print("image", image, "noise", self.rgb_image)
+            self.grey_image = cv2.cvtColor(self.rgb_image, cv2.COLOR_BGR2GRAY)
+        except CvBridgeError as e:
+            print(e)
+
+    def check_camera_ready(self):
+        self.rgb_image = None
+        while self.rgb_image is None and not rospy.is_shutdown():
+            try:
+                data = rospy.wait_for_message(self.topic, Image, timeout=5.0)
+                image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+                self.rgb_image = self._guass_noisy(image, self.noise)
+                rospy.logdebug("Current image READY=>")
+            except:
+                rospy.logerr("Current image not ready yet, retrying for getting image")
+
+    def image_arr(self):
+        img = cv2.resize(self.rgb_image, self.resolution)
+        # normalize the image for easier training
+        img_arr = np.array(img)/255 - 0.5
+        img_arr = img_arr.reshape((64,64,3))
+        return img_arr
+
+    def grey_arr(self):
+        img = cv2.resize(self.grey_image, self.resolution)
+        # normalize the image for easier training
+        img_arr = np.array(img)/255 - 0.5
+        img_arr = img_arr.reshape((64,64,1))
+        return img_arr
+
+    def _guass_noisy(self,image,var):
+        if var == 0.0:
+            return image
+        img = skimage.util.img_as_float(image)
+        noisy = skimage.util.random_noise(img,'gaussian',mean=0.0,var=var)
+        return skimage.util.img_as_ubyte(noisy)
+
+    def _noisy(self,image,type):
+        if type == 'guassian':
+            img = skimage.util.img_as_float(image)
+            noisy = skimage.util.random_noise(img,'gaussian')
+            res = skimage.util.img_as_ubyte(noisy)
+        elif type == 'salt':
+            img = skimage.util.img_as_float(image)
+            noisy = skimage.util.random_noise(img,'salt')
+            res = skimage.util.img_as_ubyte(noisy)
+        elif type == 'pepper':
+            img = skimage.util.img_as_float(image)
+            noisy = skimage.util.random_noise(img,'pepper')
+            res = skimage.util.img_as_ubyte(noisy)
+        elif type == 'poisson':
+            img = skimage.util.img_as_float(image)
+            noisy = skimage.util.random_noise(img,'poisson')
+            res = skimage.util.img_as_ubyte(noisy)
+        else:
+            res = image
+        return res
+
+"""
+pose sensor
+"""
+class PoseSensor():
+    def __init__(self, noise=0.0):
+        self.noise = noise
+        self.door_pose_sub = rospy.Subscriber('/gazebo/link_states', LinkStates, self._door_pose_cb)
+        self.robot_pos_sub = rospy.Subscriber('/gazebo/model_states', ModelStates, self._robot_pose_cb)
+        self.robot_pose = None
+        self.door_pose = None
+
+    def _door_pose_cb(self,data):
+        index = data.name.index('hinged_door::door')
+        self.door_pose = data.pose[index]
+
+    def _robot_pose_cb(self,data):
+        index = data.name.index('mobile_robot')
+        self.robot_pose = data.pose[index]
+
+    def robot(self):
+        # add noise
+        return self.robot_pose
+
+    def door(self):
+        return self.door_pose
+
+    def check_sensor_ready(self):
+        self.robot_pose = None
+        rospy.logdebug("Waiting for /gazebo/model_states to be READY...")
+        while self.robot_pose is None and not rospy.is_shutdown():
+            try:
+                data = rospy.wait_for_message("/gazebo/model_states", ModelStates, timeout=5.0)
+                index = data.name.index('mobile_robot')
+                self.robot_pose = data.pose[index]
+                rospy.logdebug("Current  /gazebo/model_states READY=>")
+            except:
+                rospy.logerr("Current  /gazebo/model_states not ready yet, retrying for getting  /gazebo/model_states")
+
+        self.door_pose = None
+        rospy.logdebug("Waiting for /gazebo/link_states to be READY...")
+        while self.door_pose is None and not rospy.is_shutdown():
+            try:
+                data = rospy.wait_for_message("/gazebo/link_states", LinkStates, timeout=5.0)
+                index = data.name.index('hinged_door::door')
+                self.door_pose = data.pose[index]
+                rospy.logdebug("Current  /gazebo/link_states READY=>")
+            except:
+                rospy.logerr("Current  /gazebo/link_states not ready yet, retrying for getting  /gazebo/link_states")
+
+"""
+Robot Driver
+"""
+class RobotDriver():
+    def __init__(self):
+        self.vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+    def drive(self,vx,vyaw):
+        msg = Twist()
+        msg.linear.x = vx
+        msg.linear.y = 0
+        msg.linear.z = 0
+        msg.angular.x = 0
+        msg.angular.y = 0
+        msg.angular.z = vyaw
+        self.vel_pub.publish(msg)
+        # add noise
+
+    def stop(self):
+        self.drive(0,0)
+
+    def check_connection(self):
+      rate = rospy.Rate(10)  # 10hz
+      while self.vel_pub.get_num_connections() == 0 and not rospy.is_shutdown():
+        rospy.logdebug("No susbribers to vel_pub yet so we wait and try again")
+        try:
+          rate.sleep()
+        except rospy.ROSInterruptException:
+          # This is to avoid error when world is rested, time when backwards.
+          pass
+      rospy.logdebug("vel_pub Publisher Connected")
+
+###############################################################################
 register(
   id='DoorOpenTash-v0',
   entry_point='envs.door_open_task_env:DoorOpenTaskEnv')
@@ -44,10 +202,9 @@ class DoorOpenTaskEnv(GymGazeboEnv):
       reset_world_or_sim="WORLD"
     )
 
-    self.info = {}
     self.door_dim = [0.9144, 0.0698] # length, width
-    self.action_space = self._action_space()
-
+    self.info = {}
+    self.action_space = 2*np.array([[1.5,3.14],[1.5,0.0],[0.0,3.14],[-1.5,3.14],[-1.5,0.0],[1.5,-3.14],[0.0,-3.14],[-1.5,-3.14]]) # x and yaw velocities
     rospy.logdebug("Start DoorOpenTaskEnv INIT...")
     self.gazebo.unpauseSim()
 
@@ -55,8 +212,8 @@ class DoorOpenTaskEnv(GymGazeboEnv):
     self.back_camera = CameraSensor(resolution,'/cam_back/image_raw',cam_noise)
     self.up_camera = CameraSensor(resolution,'/cam_up/image_raw',cam_noise)
 
-    self.pose_sensor = PoseSensor()
     self.driver = RobotDriver()
+    self.pose_sensor = PoseSensor()
 
     self._check_all_sensors_ready()
     self.robot_pose_pub = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=1)
@@ -65,17 +222,8 @@ class DoorOpenTaskEnv(GymGazeboEnv):
     self.gazebo.pauseSim()
     rospy.logdebug("Finished DoorOpenTaskEnv INIT...")
 
-  # a safe velocity of robot is 1.5 m/s, given a radius of 0.5 m robot, the safe angular
-  # velocity can be pi rad/s
-  def _action_space(self):
-      lv,av = 1.5,3.14
-      action_space = 2*np.array([[lv,av],[lv,0],[0,av],[-lv,av],[-lv,0],[lv,-av],[0,-av],[-lv,-av]])
-      print("action space", action_space)
-      return action_space
-
   def action_dimension(self):
       dim = self.action_space.shape
-      print("action space dimension", dim[0])
       return dim[0]
 
   def _check_all_systems_ready(self):
@@ -94,7 +242,7 @@ class DoorOpenTaskEnv(GymGazeboEnv):
     rospy.logdebug("All Sensors READY")
 
   def _check_publisher_connection(self):
-    self.driver.check_driver_ready()
+    self.driver.check_connection()
     rospy.logdebug("All Publishers READY")
 
   def _get_observation(self):
@@ -176,6 +324,41 @@ class DoorOpenTaskEnv(GymGazeboEnv):
       else:
           return False
 
+  # camera position in door polar coordinate frame
+  # return radius to (0,0) and angle 0 for (0,1,0)
+  def _camera_position(self):
+      cam_pose = self._robot_footprint_position(0.5,-0.25)
+      angle = math.atan2(cam_pose[0,3],cam_pose[1,3])
+      radius = math.sqrt(cam_pose[0,3]*cam_pose[0,3]+cam_pose[1,3]*cam_pose[1,3])
+      return radius, angle
+
+  # door position in polar coordinate frame
+  # retuen radius to (0,0) and angle 0 for (0,1,0)
+  def _door_position(self):
+      door_matrix = self._pose_matrix(self.pose_sensor.door())
+      door_edge = np.array([[1,0,0,self.door_dim[0]],
+                            [0,1,0,0],
+                            [0,0,1,0],
+                            [0,0,0,1]])
+      door_edge_mat = np.dot(door_matrix, door_edge)
+      # open angle [0, pi/2]
+      open_angle = math.atan2(door_edge_mat[0,3],door_edge_mat[1,3])
+      return self.door_dim[0], open_angle
+
+
+  # robot is out of the door way (x < 0)
+  def _robot_is_out(self):
+      # footprint of robot
+      fp_lf = self._robot_footprint_position(0.25,0.25)
+      fp_lr = self._robot_footprint_position(-0.25,0.25)
+      fp_rf = self._robot_footprint_position(0.25,-0.25)
+      fp_rr = self._robot_footprint_position(-0.25,-0.25)
+      cam_p = self._robot_footprint_position(0.5,-0.25)
+      if fp_lf[0,3] < 0.0 and fp_lr[0,3] < 0.0 and fp_rf[0,3] < 0.0 and fp_rr[0,3] < 0.0 and cam_p[0,3] < 0.0:
+          return True
+      else:
+          return False
+
   def _robot_is_in(self):
       # footprint of robot
       dr, da = self._door_position()
@@ -190,19 +373,6 @@ class DoorOpenTaskEnv(GymGazeboEnv):
       else:
           return False
 
-  # robot is out of the door way (x < 0)
-  def _robot_is_out(self):
-     # footprint of robot
-     fp_lf = self._robot_footprint_position(0.25,0.25)
-     fp_lr = self._robot_footprint_position(-0.25,0.25)
-     fp_rf = self._robot_footprint_position(0.25,-0.25)
-     fp_rr = self._robot_footprint_position(-0.25,-0.25)
-     cam_p = self._robot_footprint_position(0.5,-0.25)
-     if fp_lf[0,3] < 0.0 and fp_lr[0,3] < 0.0 and fp_rf[0,3] < 0.0 and fp_rr[0,3] < 0.0 and cam_p[0,3] < 0.0:
-         return True
-     else:
-         return False
-
   # utility function
   def _robot_footprint_position(self,x,y):
       robot_matrix = self._pose_matrix(self.pose_sensor.robot())
@@ -212,27 +382,6 @@ class DoorOpenTaskEnv(GymGazeboEnv):
                                [0,0,0,1]])
       fp_mat = np.dot(robot_matrix, footprint_trans)
       return fp_mat
-
-  # camera position in door polar coordinate frame
-  # return radius to (0,0) and angle 0 for (0,1,0)
-  def _camera_position(self):
-     cam_pose = self._robot_footprint_position(0.5,-0.25)
-     angle = math.atan2(cam_pose[0,3],cam_pose[1,3])
-     radius = math.sqrt(cam_pose[0,3]*cam_pose[0,3]+cam_pose[1,3]*cam_pose[1,3])
-     return radius, angle
-
-  # door position in polar coordinate frame
-  # retuen radius to (0,0) and angle 0 for (0,1,0)
-  def _door_position(self):
-     door_matrix = self._pose_matrix(self.pose_sensor.door())
-     door_edge = np.array([[1,0,0,self.door_dim[0]],
-                           [0,1,0,0],
-                           [0,0,1,0],
-                           [0,0,0,1]])
-     door_edge_mat = np.dot(door_matrix, door_edge)
-     # open angle [0, pi/2]
-     open_angle = math.atan2(door_edge_mat[0,3],door_edge_mat[1,3])
-     return self.door_dim[0], open_angle
 
   # convert quaternion based pose to matrix
   def _pose_matrix(self,cp):
